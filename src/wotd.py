667e4ba9cec4a829cabf6b6d87739c15b044f8de
Mattias Andrée <maandree@kth.se>
2016-06-28 16:09:06 +0200

    Add Swedish word of the week
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- src/wotd.py
+++ src/wotd.py
@@ -1,16 +1,19 @@
 # -*- python -*-
 # See LICENSE file for copyright and license details.
 
-def wotd(abort_timer = 2):
+def wotd(lang = 'en', abort_timer = 2):
     '''
-    Get Wiktionary's English word of the day
+    Get Wiktionary's word of the day
     
+    @param   lang:str         The language code. Supported languages codes:
+                                en  English (word of the day)
+                                sv  Swedish (word of the week)
     @param   abort_timer:int  The number of seconds before giving up
     @return  :str?            The word of the day, in LaTeX, `None` on error
     '''
     from __main__ import spawn, LIBEXEC
     try:
-        text = spawn('%s/rotd-wotd' % LIBEXEC, abort_timer = abort_timer, get_stdout = True)
+        text = spawn('%s/rotd-wotd-%s' % (LIBEXEC, lang), abort_timer = abort_timer, get_stdout = True)
         text = text.decode('utf-8', 'strict')
         return text
     except:
